d206a1fff36bdd24df96192d93992f796b1251a7
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-09-06 22:15:17 +0200

    Created a collection view layout programmatically and created the label

---
+++ NumberCell.swift
@@ -0,0 +1,18 @@
+//
+//  NumberCell.swift
+//  CollectionView
+//
+//  Created by Pat on 2022/09/06.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+    //using the type Name as a reuse identifier
+    
+    //creates a string that matches the type name the same as the identifier
+    static let reuseIdentifier = String(describing: NumberCell.self)
+    
+    @IBOutlet var label: UILabel!
+    
+}
